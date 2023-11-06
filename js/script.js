e412c96e12fb8327b00900f78dd4a6ec9c25de9b
Duy Thai <sb23092002@gmail.com>
2023-11-06 11:09:10 +0700

    update Nov6th

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // ------------------------MENU SLIDEBAR CATEGORY--------------------------------
 
-const itemsSlideBar = document.querySelectorAll("category-left-li")
+const itemsSlideBar = document.querySelectorAll(".category-left-li")
 itemsSlideBar.forEach(function(menu,index){
     menu.addEventListener("click",function(){
+        console.log()
         menu.classList.toggle("block")
     })
 })
